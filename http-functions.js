bd6c60e7e2b8b3a569c717b7bad829a56cfe4cca
Steamdiscoveryacademy <mike@steamdiscoveryacademy.com>
2021-02-27 11:43:13 -0600

    SWITCH gone

--- http-functions.js
+++ http-functions.js
@@ -1,26 +1,15 @@
 
-// import {ok, badRequest} from 'wix-http-functions'; // ORIGINAL
-import {ok, notFound, serverError} from 'wix-http-functions'; //YOUTUBE:v=4yCBplV3MPQ&t=1s
+import { ok, notFound, serverError } from 'wix-http-functions'; //YOUTUBE:v=4yCBplV3MPQ&t=1s
 // URL to call this HTTP function from your published site looks like:
 // Premium site - https://mysite.com/_functions/example/multiply?leftOperand=3&rightOperand=4
-// Free site - https://username.wixsite.com/mysite/_functions/example/multiply?leftOperand=3&rightOperand=4
-// Free site - https://brad7390.wixsite.com/my-site-2/_functions/freeLessonRequest/multiply?leftOperand=3&rightOperand=4
 // Free site - https://brad7390.wixsite.com/my-site-2/_functions/freeLessonRequest
-            // https://brad7390.wixsite.com/my-site-2/
 
 // URL to test this HTTP function from your saved site looks like:
 // Premium site - https://mysite.com/_functions-dev/example/multiply?leftOperand=3&rightOperand=4
-// Free site - https://username.wixsite.com/mysite/_functions-dev/example/multiply?leftOperand=3&rightOperand=4
-// Free site - https://brad7390.wixsite.com/my-site-2/_functions-dev/freeLessonRequest/multiply?leftOperand=3&rightOperand=4
 // Free site - https://brad7390.wixsite.com/my-site-2/_functions-dev/freeLessonRequest
 
 // export function get_freeLessonRequest(request) {
 export function post_freeLessonRequest(request) {
-	// const response = {
-	// 	"headers": {
-	// 		"Content-Type": "application/json"
-	// 	}
-	// };
 	let options = {
 		"headers": {
 			"Content-Type": "application/json"
@@ -28,30 +17,27 @@ export function post_freeLessonRequest(request) {
 	};
 
 
-	const idFormStack = 'free_lesson_request';
+	const title = 'freeLessonRequest';
+	const fsFormId = '4262311';
+	const manualHandshake = '8FD7E76007870C3F4352D478B19E5AE6E61672918EBDEADC023491A7ABBE8197';
+	const verifyHandshake = '8FD7E76007870C3F4352D478B19E5AE6E61672918EBDEADC023491A7ABBE8197';
+	const handshakeMatch = manualHandshake === verifyHandshake ? true : false;
 	// Perform the requested operation and return an OK response
 	// with the answer in the response body
-	switch (idFormStack) {
-	case 'free_lesson_request':
+	if (handshakeMatch) {
 		// response.body = {
 		// 	"success": "got it FormStack"
 		// };
 		return request.body.json()
-		.then( (body) => {
-			// insert the item in a collection
+			.then((body) => {
+				// insert the item in a collection
 
-    	console.log('free_lesson_request Received');
-		console.log(body);
-		// console.log(request.payload);
-		return ok(options);
-  	} )
-	case 'multiply':
-		// response.body = {
-		// 	"product": leftOperand * rightOperand
-		// };
-		// return ok(response);
-		return ok(options);
-	default:
+				console.log('free_lesson_request Received');
+				console.log(body);
+				console.log(body.HandshakeKey);
+				return ok(options);
+			})
+	} else {
 		// If the requested operation was not found, return a Bad Request
 		// response with an error message in the response body
 		// response.body = {
@@ -61,4 +47,11 @@ export function post_freeLessonRequest(request) {
 		console.warn('free_lesson_request Failed');
 		return notFound(options);
 	}
+}
+
+export function webhookPayload_INSERT(title, body, timestamp) {
+	const insertTableName = 'webhookPayload';
+	const insertFieldNames = ['title', 'payload', 'webhookStamp', 'webhookId', 'currentStatus', 'currentStatusStamp', 'resolvedStatus', 'resolvedStatusStamp'];
+	let payload = body;
+
 }
