10dcdbbb0f566e6dd40c177f8d4ec4d4701e706c
ferdely <bradlowry@gmail.com>
2021-05-01 12:40:17 -0500

    woops, never committed contactFun (page js)

--- wixContact_instantiateObjectPaths_fromRuMe.js
+++ wixContact_instantiateObjectPaths_fromRuMe.js
@@ -2,9 +2,14 @@ let wixContact = {};
 let error = false;
 let errorString = '';
 // let elementKind = 'ppendingKind';
+//<Single for Testing>
+ruMeUserJSON = "{\"gender\":\"female\",\"name\":{\"title\":\"Ms\",\"first\":\"Valerie\",\"last\":\"Steeves \"},\"location\":{\"street\":{\"number\":7298,\"name\":\"Cackson St\"},\"city\":\"Everett\",\"state\":\"Minnesota\",\"country\":\"US\",\"postcode\":21730,\"coordinates\":{\"latitude\":\"72.8313\",\"longitude\":\"88.7922\"},\"timezone\":{\"offset\":\"+9:30\",\"description\":\"Adelaide, Darwin\"},\"postalCode\":\"21730\",\"subdivision\":\"MN\"},\"email\":\"valerie.steeves@example.com\",\"login\":{\"uuid\":\"fdfeb147-2664-41da-be4d-c288cd41a607\",\"username\":\"tinyrabbit249\",\"password\":\"desiree\",\"salt\":\"zxre1N03\",\"md5\":\"927b51f40b779d85e934af89bb0b7138\",\"sha1\":\"aa5cce067b10d88f6fce407f0f023b3381636f76\",\"sha256\":\"071b0a9928c8a27a1e6bb3d5a96b1aa5591ad75cd677534b27ac993a40a157db\"},\"dob\":{\"date\":\"1974-07-12T21:56:46.101Z\",\"age\":47},\"registered\":{\"date\":\"2011-12-28T23:57:15.997Z\",\"age\":10},\"phone\":\"(820)-630-4825\",\"cell\":\"(773)-727-8548\",\"id\":{\"name\":\"SSN\",\"value\":\"459-06-3203\"},\"picture\":{\"large\":\"https://randomuser.me/api/portraits/women/76.jpg\",\"medium\":\"https://randomuser.me/api/portraits/med/women/76.jpg\",\"thumbnail\":\"https://randomuser.me/api/portraits/thumb/women/76.jpg\"},\"nat\":\"US\",\"seed\":\"67e360b44a4523de\",\"info\":[],\"birthdate\":\"1974-07-12\"}";
+ruMeUser = JSON.parse(ruMeUserJSON);
+// console.log(ruMeUser);
+//</Single for Testing>
 
 //<likely Parameterized>
-// let pathArray = ['name.first', 'name.last', 'addresses.address.city']
+let assinmentPathArray = [["last", "info.name.last"],["first", "info.name.first"]];
 //</likely Parameterized>
 
 //<relatively Konstant>
@@ -63,12 +68,12 @@ for (index = 0; index < supportedElementKeyArray.length; index++) {
     }
 }
 
-// pathArray = [["last", "info.name.last"],["first", "info.name.first"]];
-pathArray = [["first","info.name.first","testing"],["last","info.name.last","testing"],["company","info.company","testing"],["jobTitle","info.jobTitle","testing"],["locale","info.locale","testing"],["birthdate","info.birthdate","testing"],["tag","info.emails[0].tag","testing"],["email","info.emails[0].email","testing"],["primary","info.emails[0].primary","testing"],["tag","info.phones[0].tag","testing"],["phone","info.phones[0].phone","testing"],["primary","info.phones[0].primary","testing"],["tag","info.addresses[0].tag","testing"],["formatted","info.addresses[0].address.formatted","testing"],["number","info.addresses[0].address.streetAddress.number","testing"],["name","info.addresses[0].address.streetAddress.name","testing"],["addressLine2","info.addresses[0].address.addressLine2","testing"],["city","info.addresses[0].address.city","testing"],["subdivision","info.addresses[0].address.subdivision","testing"],["postalCode","info.addresses[0].address.postalCode","testing"],["country","info.addresses[0].address.country","testing"],["latitude","info.addresses[0].address.location.latitude","testing"],["longitude","info.addresses[0].address.location.longitude","testing"]];
+// assinmentPathArray = [["last", "info.name.last"],["first", "info.name.first"]];
+// assinmentPathArray = [["first","info.name.first","testing"],["last","info.name.last","testing"],["company","info.company","testing"],["jobTitle","info.jobTitle","testing"],["locale","info.locale","testing"],["birthdate","info.birthdate","testing"],["tag","info.emails[0].tag","testing"],["email","info.emails[0].email","testing"],["primary","info.emails[0].primary","testing"],["tag","info.phones[0].tag","testing"],["phone","info.phones[0].phone","testing"],["primary","info.phones[0].primary","testing"],["tag","info.addresses[0].tag","testing"],["formatted","info.addresses[0].address.formatted","testing"],["number","info.addresses[0].address.streetAddress.number","testing"],["name","info.addresses[0].address.streetAddress.name","testing"],["addressLine2","info.addresses[0].address.addressLine2","testing"],["city","info.addresses[0].address.city","testing"],["subdivision","info.addresses[0].address.subdivision","testing"],["postalCode","info.addresses[0].address.postalCode","testing"],["country","info.addresses[0].address.country","testing"],["latitude","info.addresses[0].address.location.latitude","testing"],["longitude","info.addresses[0].address.location.longitude","testing"]];
 let value = '';
-breakOn = 22;
-for (index = 0; index < pathArray.length; index++) {
-    element = pathArray[index];
+breakOn = 0;
+for (index = 0; index < assinmentPathArray.length; index++) {
+    element = assinmentPathArray[index];
     name = element[0];
     path = element[1];
     // value = element[2];
