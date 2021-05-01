a18079e4afea6358c9b751223303c998f189cbaf
ferdely <bradlowry@gmail.com>
2021-05-01 17:36:58 -0500

    tweaks to additional code

--- contactFun.js
+++ contactFun.js
@@ -293,21 +293,20 @@ async function doRandomUser(paramObject = {}){
 	resolvedResponse.location.country = resolvedResponse.location.country === 'United States' ? 'US' : resolvedResponse.location.country;
 	// resolvedResponse.info = [];
 	let transformedAttributes = [];//[['locale','literal'],['postalCode','ensure 5 and string'],[['birthdate'],['dob.date to left 10'],[['subdivision'],['2letter vesion of "state" (not removed)'],[['Company'],['Street name + random business kind'],[['jobTitle'],['random jobTitle']]
-	resolvedResponse.developer = {};
 	resolvedResponse.locale = 'en-US';
-	transformedAttributes.push(['locale','literal en-US']);
+	transformedAttributes.push(['locale',"literal 'en-US'"]);
 	let postalCode = '00000' + resolvedResponse.location.postcode.toString();
 	postalCode = postalCode.substr(-5);
 	resolvedResponse.location.postalCode = postalCode;
-	transformedAttributes.push(['postalCode','ensure 5 and string left postcode as number']);
+	transformedAttributes.push(['postalCode','ensure 5 and string - left postcode as number']);
 	let birthdate = resolvedResponse.dob.date.substr(0,10);
 	resolvedResponse.birthdate = birthdate;
-	transformedAttributes.push(['birthdate'],['dob.date to left 10']);
+	transformedAttributes.push(['birthdate','dob.date to left 10. no change to original']);
 	let stateAbbrvObject = {"Alabama":"AL","Alaska":"AK","American Samoa":"AS","Arizona":"AZ","Arkansas":"AR","California":"CA","Colorado":"CO","Connecticut":"CT","Delaware":"DE","District Of Columbia":"DC","Federated States Of Micronesia":"FM","Florida":"FL","Georgia":"GA","Guam":"GU","Hawaii":"HI","Idaho":"ID","Illinois":"IL","Indiana":"IN","Iowa":"IA","Kansas":"KS","Kentucky":"KY","Louisiana":"LA","Maine":"ME","Marshall Islands":"MH","Maryland":"MD","Massachusetts":"MA","Michigan":"MI","Minnesota":"MN","Mississippi":"MS","Missouri":"MO","Montana":"MT","Nebraska":"NE","Nevada":"NV","New Hampshire":"NH","New Jersey":"NJ","New Mexico":"NM","New York":"NY","North Carolina":"NC","North Dakota":"ND","Northern Mariana Islands":"MP","Ohio":"OH","Oklahoma":"OK","Oregon":"OR","Palau":"PW","Pennsylvania":"PA","Puerto Rico":"PR","Rhode Island":"RI","South Carolina":"SC","South Dakota":"SD","Tennessee":"TN","Texas":"TX","Utah":"UT","Vermont":"VT","Virgin Islands":"VI","Virginia":"VA","Washington":"WA","West Virginia":"WV","Wisconsin":"WI","Wyoming":"WY"};
 	let state = resolvedResponse.location.state
 	let subdivision = stateAbbrvObject[state];
 	resolvedResponse.location.subdivision = subdivision;
-	transformedAttributes.push(['subdivision','2letter vesion of "state" (not removed)']);
+	transformedAttributes.push(['subdivision',"2letter version of 'state' - original not removed"]);
 	let company = resolvedResponse.location.street.name;
 	company = company.substr(0,company.indexOf(" "));//first word, how about all but last, but pretty good
 	let bussnessKindArray = ['Incorporated','Limited','Corporation','University','College','Auto Parts','Associates','Restaurant','Stores'];
@@ -318,9 +317,19 @@ async function doRandomUser(paramObject = {}){
 	let jobTitle = jobTitleArray[Math.floor(Math.random() * jobTitleArray.length)];
 	resolvedResponse.jobTitle = jobTitle;
 	transformedAttributes.push(['jobTitle','random jobTitle']);
+	let emailPrimary = 'qiqgroup+' + resolvedResponse.name.first.toLowerCase() + '@gmail.com';
+	resolvedResponse.emailPrimary = emailPrimary;
+	transformedAttributes.push(['emailPrimary','qiqgroup + firstName - real so that enrollment happens']);
+	let addressLine2TypeArray = ['#','Suite','Box']
+	let addressLine2 = Math.floor(Math.random() * 1000).toString();
+	let addressLine2Type = Number(addressLine2) < 40 ? 'Floor' : addressLine2TypeArray[Math.floor(Math.random() * addressLine2TypeArray.length)];
+	addressLine2 = addressLine2Type + ' ' + addressLine2;
+	resolvedResponse.location.addressLine2 = addressLine2;
+	transformedAttributes.push(['addressLine2','random number and random type']);
 	// console.log(('transformedAttributes: '))
 	// console.log((transformedAttributes))
 	// let transformedAttributes = [['locale','literal'],['postalCode','ensure 5 and string'],[['birthdate'],['dob.date to left 10'],[['subdivision'],['2letter vesion of "state" (not removed)'],[['Company'],['Street name + random business kind'],[['jobTitle'],['random jobTitle']]
+	resolvedResponse.developer = {};
 	resolvedResponse.developer.transformedAttributes = transformedAttributes;
 
 	console.log("ONE.b: resolvedResponse after 'await getJSON'");
