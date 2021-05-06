bb36bc804030432e4197f5682f14e89fd27250d5
ferdely <bradlowry@gmail.com>
2021-05-05 19:10:26 -0500

    much improvement, array,filter() to remove nulls rather than fuss with indexes.

--- contacts/wixContact_codeFromRuMe_homeStretch.js
+++ contacts/wixContact_codeFromRuMe_homeStretch.js
@@ -2,11 +2,6 @@ let wixContact = {};
 let error = false;
 let errorString = '';
 let holderString = '';
-//<evalTestString>
-// let evalTestString = "wixContact['info'] = {}";
-// eval(evalTestString);
-// errorString += '';
-//</evalTestString>
 
 //<UI checkbox emulation>
 // let chkbxKeyArray = ['NNULL', 'first', 'last', 'company', 'jobTitle', 'locale', 'birthdate', 'email', 'emailP', 'phoneP', 'phone', 'address'];
@@ -15,10 +10,10 @@ let holderString = '';
 let chkbxKeyArray = [];
 chkbxKeyArray.push('first');
 chkbxKeyArray.push('last');
-// chkbxKeyArray.push('company');
-// chkbxKeyArray.push('jobTitle');
-// chkbxKeyArray.push('locale');
-// chkbxKeyArray.push('birthdate');
+chkbxKeyArray.push('company');
+chkbxKeyArray.push('jobTitle');
+chkbxKeyArray.push('locale');
+chkbxKeyArray.push('birthdate');
 chkbxKeyArray.push('email');
 chkbxKeyArray.push('emailP');
 // chkbxKeyArray.push('phoneP');
@@ -47,12 +42,7 @@ console.log(chkbxKeyArray);
 //</UI checkbox emulation>
 
 //<Single for Testing>
-// ruMeUserJSON = "{\"gender\":\"female\",\"name\":{\"title\":\"Ms\",\"first\":\"Valerie\",\"last\":\"Steeves \"},\"location\":{\"street\":{\"number\":7298,\"name\":\"Cackson St\"},\"city\":\"Everett\",\"state\":\"Minnesota\",\"country\":\"US\",\"postcode\":21730,\"coordinates\":{\"latitude\":\"72.8313\",\"longitude\":\"88.7922\"},\"timezone\":{\"offset\":\"+9:30\",\"description\":\"Adelaide, Darwin\"},\"postalCode\":\"21730\",\"subdivision\":\"MN\"},\"email\":\"valerie.steeves@example.com\",\"login\":{\"uuid\":\"fdfeb147-2664-41da-be4d-c288cd41a607\",\"username\":\"tinyrabbit249\",\"password\":\"desiree\",\"salt\":\"zxre1N03\",\"md5\":\"927b51f40b779d85e934af89bb0b7138\",\"sha1\":\"aa5cce067b10d88f6fce407f0f023b3381636f76\",\"sha256\":\"071b0a9928c8a27a1e6bb3d5a96b1aa5591ad75cd677534b27ac993a40a157db\"},\"dob\":{\"date\":\"1974-07-12T21:56:46.101Z\",\"age\":47},\"registered\":{\"date\":\"2011-12-28T23:57:15.997Z\",\"age\":10},\"phone\":\"(820)-630-4825\",\"cell\":\"(773)-727-8548\",\"id\":{\"name\":\"SSN\",\"value\":\"459-06-3203\"},\"picture\":{\"large\":\"https://randomuser.me/api/portraits/women/76.jpg\",\"medium\":\"https://randomuser.me/api/portraits/med/women/76.jpg\",\"thumbnail\":\"https://randomuser.me/api/portraits/thumb/women/76.jpg\"},\"nat\":\"US\",\"seed\":\"67e360b44a4523de\",\"info\":[],\"birthdate\":\"1974-07-12\"}";
-// ruMeUser = JSON.parse(ruMeUserJSON);
 let ruMeUser = { gender: "male", name: { title: "Mr", first: "Floyd", last: "Holt" }, location: { street: { number: 5365, name: "Woodland St", apt: "371" }, city: "Elko", state: "California", country: "US", postcode: 13195, coordinates: { latitude: "-32.1370", longitude: "-38.8458" }, timezone: { offset: "-5:00", description: "Eastern Time (US & Canada), Bogota, Lima" }, postalCode: "13195", subdivision: "CA", addressLine2: "Box 122" }, email: "floyd.holt@example.com", login: { uuid: "5638f410-b574-4551-b262-ff4bf4ebfb87", username: "happycat971", password: "buster1", salt: "RwDqvtgU", md5: "9584ff21a6d448905719e2216eb9b976", sha1: "f684942cda707ba52d78af9c104b290d70fc392f", sha256: "fa1c0cd4a1c8927cb9a3a5d2d6b7c6004641a8174faec28643938fdf0921660a" }, dob: { date: "1996-01-15T05:11:36.209Z", age: 25 }, registered: { date: "2006-11-05T22:27:23.562Z", age: 15 }, phone: "(938)-796-5414", cell: "(315)-781-5760", id: { name: "SSN", value: "364-38-2775" }, picture: { large: "https://randomuser.me/api/portraits/men/71.jpg", medium: "https://randomuser.me/api/portraits/med/men/71.jpg", thumbnail: "https://randomuser.me/api/portraits/thumb/men/71.jpg" }, nat: "US", seed: "3628bfbbd09c27aa", locale: "en-US", birthdate: "1996-01-15", company: "Woodland College", jobTitle: "Supervisor", emailPrimary: "qiqgroup+floyd@gmail.com" };
-// let evalStringTest = "ruMeUser.name.first";
-// let evalStringEvalTest = eval(evalStringTest);
-// console.log(ruMeUser);
 //</Single for Testing>
 
 //<likely Parameterized>
@@ -71,10 +61,8 @@ let doxKeyElement = assinmentPathArray[0];
 let copystring = doxKeyElement.toString()
 if (doxKeyElement.toString() === "KEY.name,KEY.path,KEY.index,KEY.ruMePath,KEY.ruMeCode,KEY.uiKey") {
     doxKeyElement = assinmentPathArray.shift();
-    // console.log(doxKeyElement.toString());
 }
 let element = assinmentPathArray[0];
-let clElement = "name:'" + element[0] + "',path:'" + element[1] + "', index:'" + element[2] + "', ruMePath:'" + element[3] + "', ruMeCode:'" + element[4] + "'";
 let name = '';
 let path = '';
 let index = '-777';
@@ -84,27 +72,8 @@ let uiKey = '';
 let value = '';
 let processedPathArray = [];
 let evalString = '';
-// let swEmptyPathCount = 0;
-// let swUnchkdCount = 0;
-// let swInstObjCount = 0;
-// let swIndxICount = 0;
-// let swInstArrCount = 0;
-// let swInstElmtObjCount = 0;
-// let swNullPathCount = 0;
-// let swDfltAssigCount = 0;
-
-let emailIndex = 0;
-let isEmail = false;
-let phoneIndex = 0;
-let isPhone = false;
-let addressIndex = 0;
-let isAddress = false;
 for (let forIndex = 0; forIndex < assinmentPathArray.length; forIndex++) {
     element = assinmentPathArray[forIndex];
-    clElement = "name:'" + element[0] + "',path:'" + element[1] + "', index:'" + element[2] + "', ruMePath:'" + element[3] + "', ruMeCode:'" + element[4] + "'";
-    let forIndexNext = forIndex + 1;
-    // elementNext = assinmentPathArray[forIndexNext];
-    // clElementNext = "name:'" + elementNext[0] + "',path:'" + elementNext[1] + "', index:'" + elementNext[2] + "', ruMePath:'" + elementNext[3] + "', ruMeCode:'" + elementNext[4] + "'";
     name = element[0];
     path = element[1];
     index = element[2].toString();
@@ -125,27 +94,17 @@ for (let forIndex = 0; forIndex < assinmentPathArray.length; forIndex++) {
     let indexReplaceString = '';
     let pathSplit = [];
     holderString = "About to Enter SWITCH: ${swDfltAssigCount} Assigned"
-    //<most for 'default', but could be useful for other reasons>
-    //  \_ there may be other indications, but these seem most direct
-    isEmail = path === "info.emails.email" ? true : false;
-    //</most for 'default', but could be useful for other reasons>
     switch (true) {
         case ruMePath.length === 0:
-            // swEmptyPathCount++;
-            console.warn("'path' must he 'NNULL' or a valid path.")
-            console.warn(clElement);
             break;
+
         case uiKeyUnChecked:
-            // swUnchkdCount++;
             break;
 
         case index === 'i':
-            // swIndxICount++;
             break;
 
         case name === 'INSTANTIATE_OBJECT':
-            // swInstObjCount++;
-
             attributeChain = 'wixContact';
             indexReplaceString = '.' + index + '';
             path = path.replace('[i]', indexReplaceString);
@@ -163,13 +122,9 @@ for (let forIndex = 0; forIndex < assinmentPathArray.length; forIndex++) {
                 console.warn("attributeChain: " + attributeChain);
             }
             eval(attributeChain);
-            // element.unshift('INSTANTIATE_OBJECT');
-            // element.unshift(forIndex);
-            // processedPathArray.push(element.toString());
             break;
 
         case name === 'INSTANTIATE_ARRAY':
-            // swInstArrCount++;
             attributeChain = 'wixContact';
             pathSplit = path.split(".");
             pathSplit.forEach(chunk => {
@@ -177,28 +132,15 @@ for (let forIndex = 0; forIndex < assinmentPathArray.length; forIndex++) {
             });
             attributeChain += ' = []';
             eval(attributeChain);
-            // element.unshift('INSTANTIATE_OBJECT');
-            // element.unshift(forIndex);
-            // processedPathArray.push(element.toString());
             break;
 
         case name === 'INSTANTIATE_ELEMENT_OBJECT':
-            // swInstElmtObjCount++;
-
-            // console.warn("name === 'INSTANTIATE_ELEMENT_OBJECT'")
-            // console.warn(clElement);
             break;
 
         case ruMePath === 'NNULL':
-            // swNullPathCount++;
             break;
 
         default:
-            // swDfltAssigCount++;
-            // indexReplaceString = '[' + index + ']';
-            // path = path.replace('[i]', indexReplaceString);
-            // index = isEmail ? emailIndex : index;
-            // emailIndex += isEmail ? 1 : 0;
             indexReplaceString = '[' + index + '].';
             path = path.replace('.i.', indexReplaceString);
             if (ruMePath === 'NNA') {
@@ -217,46 +159,9 @@ for (let forIndex = 0; forIndex < assinmentPathArray.length; forIndex++) {
 
 }
 
-let holderCompactArray = [];
-holderCompactArray = compactArray(wixContact.info.emails);
-wixContact.info.emails = [];
-wixContact.info.emails = holderCompactArray;
-holderCompactArray = [];
-holderCompactArray = compactArray(wixContact.info.phones);
-wixContact.info.phones = [];
-wixContact.info.phones = holderCompactArray;
-holderCompactArray = [];
-holderCompactArray = compactArray(wixContact.info.addresses);
-wixContact.info.addresses = [];
-wixContact.info.addresses = holderCompactArray;
+let originalArray = wixContact.info.emails;
+wixContact.info.emails = originalArray.filter(element => element !== null && element !== undefined);
 
-export function compactArray(originalArray = []) {
-    if (Array.isArray(originalArray) === false) {
-        return;
-    }
-    // let holderArray = wixContact.info.emails;
-    let compactArray = [];
-    for (let index = 0; index < originalArray.length; index++) {
-        const element = originalArray[index];
-        if (element !== null && element !== undefined) {
-            compactArray.push(element);
-            // console.log(element);
-        }
-    }
-    return compactArray;
-}
-
-// let testOriginalArray = ['alpah','bravo',null,'delta',null,null,null,null,null,'juliet',null,null,'november',null,null,null,'romeo']
-// let testCompactArray = compactArray(testOriginalArray);
-// console.log(testCompactArray); 
-// list = [null, "say", "kenglish", "co", null]
-// list.filter(function(obj) { return obj });
-// Or with es6
-// list.filter((obj) => obj );
-// list.filter((obj) => obj );
-// console.log(list);
-// return new array [ 'say',
-// wixContact.info.emails.filter((obj) => obj );
 console.log(JSON.stringify(wixContact, undefined, 4))
 errorString += 'DEBUG STEPS';
 errorString = '';
