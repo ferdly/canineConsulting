1411e1f199d7371df5b3013b615bd986bda0b12a
ferdely <bradlowry@gmail.com>
2021-05-19 17:05:44 -0500

    skinny flat change for labelKeys adjustment

--- contacts/agile/sprint/doRemoveCheckedFromPrep.js
+++ contacts/agile/sprint/doRemoveCheckedFromPrep.js
@@ -9,9 +9,53 @@ export function doRemoveCheckedFromPrep() {
         // checkboxArray.forEach(element => {
         console.log('Overall: ' + removeByAction + ': ' + element);
         switch (element) {
-            case 'value':
+            case 'name':
                 if (removeByAction === 'DELETE') {
+                    console.log(removeByAction + '[testing]: ' + element);
+                } else if (removeByAction === 'EMPTY') {
                     console.log(removeByAction + ': ' + element);
+                } else {
+                    console.log(removeByAction + ': ' + element);
+                    console.log('DEFAULT: Switch does NOT support the action for the element: element & action: [' + element + ' & ' + removeByAction + ']');
+                }
+                break;
+
+            case 'nonname':
+                if (removeByAction === 'DELETE') {
+                    console.log(removeByAction + '[testing]: ' + element);
+                } else if (removeByAction === 'EMPTY') {
+                    console.log(removeByAction + ': ' + element);
+                } else {
+                    console.log(removeByAction + ': ' + element);
+                    console.log('DEFAULT: Switch does NOT support the action for the element: element & action: [' + element + ' & ' + removeByAction + ']');
+                }
+                break;
+
+            case 'emails':
+                if (removeByAction === 'DELETE') {
+                    console.log(removeByAction + '[testing]: ' + element);
+                } else if (removeByAction === 'EMPTY') {
+                    console.log(removeByAction + ': ' + element);
+                } else {
+                    console.log(removeByAction + ': ' + element);
+                    console.log('DEFAULT: Switch does NOT support the action for the element: element & action: [' + element + ' & ' + removeByAction + ']');
+                }
+                break;
+				
+            case 'phones':
+                if (removeByAction === 'DELETE') {
+                    console.log(removeByAction + '[testing]: ' + element);
+                } else if (removeByAction === 'EMPTY') {
+                    console.log(removeByAction + ': ' + element);
+                } else {
+                    console.log(removeByAction + ': ' + element);
+                    console.log('DEFAULT: Switch does NOT support the action for the element: element & action: [' + element + ' & ' + removeByAction + ']');
+                }
+                break;
+            case 'addresses':
+                if (removeByAction === 'DELETE') {
+                    console.log(removeByAction + '[testing]: ' + element);
+                    delete currentContact.contactInfo.addresses;
                 } else if (removeByAction === 'EMPTY') {
                     console.log(removeByAction + ': ' + element);
                 } else {
@@ -26,6 +70,13 @@ export function doRemoveCheckedFromPrep() {
         }
     }//END for (let index = 0; index < checkboxArray.length; index++)
     $w('#codePrepContact').value = JSON.stringify(currentContact, undefined, 4);
+	let dox = [
+		['Name','name','EMPTY_OBJECT','DELETE_ELEMENT'],
+		['non-Name','nonname','EMPTY_STRING','DELETE_ELEMENT'],
+		['Emails','emails','EMPTY_ARRAY','DELETE_ELEMENT'],
+		['Phones','phones','EMPTY_ARRAY','DELETE_ELEMENT'],
+		['Addresses','addresses','EMPTY_ARRAY','DELETE_ELEMENT']
+	];
 
 }//END doRemoveCheckedFromPrep
 // ø <----------- </doRemoveCheckedFromPrep> ----------->
